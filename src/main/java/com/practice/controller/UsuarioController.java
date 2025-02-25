0e0a94d77f75687de97ee25c2ed1b699536b9546
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2025-02-25 07:52:21 -0500

    fix: update method patch for put

--- src/main/java/com/practice/controller/UsuarioController.java
+++ src/main/java/com/practice/controller/UsuarioController.java
@@ -1,28 +1,11 @@
 package com.practice.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.practice.RequestDto.UserCreateRequestDto;
-import com.practice.RequestDto.UserUpdateRequestDto;
-import com.practice.ResponseDto.UserCreateResponseDto;
-import com.practice.ResponseDto.UserPageResponseDto;
-import com.practice.ResponseDto.UserResponseDto;
-import com.practice.entity.Usuario;
+import org.springframework.web.bind.annotation.*;
+import com.practice.RequestDto.*;
+import com.practice.ResponseDto.*;
 import com.practice.service.impl.UsuarioService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -77,7 +60,7 @@ public class UsuarioController {
     @Operation(summary = "Actualiza un usuario", description = "Actualiza  un usuario por ID")
     @ApiResponse(responseCode = "200", description = "Usuario actualizado correctamente")
     @ApiResponse(responseCode = "404", description = "No se pudo actualizar el usuario")
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<UserResponseDto> updateUserById(@PathVariable Integer id, @RequestBody @Valid UserUpdateRequestDto userUpdateRequestDto) {
         UserResponseDto response = usuarioService.updateUser(id, userUpdateRequestDto);
         return new ResponseEntity<>(response, HttpStatus.OK);
